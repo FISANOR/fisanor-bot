822f40be31b6a1baa381d0fd937cc86e6b26367e
FISANOR <csgokiller5533@gmail.com>
2025-07-14 23:31:03 +0500

    ✅ Переход на asyncio для aiogram 3

--- bot.py
+++ bot.py
@@ -2,7 +2,6 @@ import logging
 import asyncio
 from aiogram import Bot, Dispatcher, types
 from aiogram.types import ReplyKeyboardMarkup, KeyboardButton, InlineKeyboardMarkup, InlineKeyboardButton
-from aiogram.utils import executor
 from aiogram.dispatcher.filters import Text
 
 API_TOKEN = '8138203975:AAE-q7SaDll1TOuFfB-inw3VsEjSowFlASM'
